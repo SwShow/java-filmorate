5e0e99699dd04e7f46abaf3cb64ab584a42b4189
Svetlana <swetlanakorowina@yandex.ru>
2022-09-20 19:41:07 +0300

    исправить аннотации и убрать зависимость

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,17 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.NonNull;
 import ru.yandex.practicum.filmorate.controller.ValidationException;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 public class Film {
 
     private int id;
+    @NotBlank
     private String name;
+    @Size(min = 1, max = 200)
     private String description;
+    @NotNull
     private LocalDate releaseDate;
+    @Min(1)
     private int duration;
 
     public Film(String name, String description, LocalDate releaseDate, int duration) {
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,15 +2,20 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 public class User {
 
     private int id;
+    @NotBlank
     private String email;
+    @NotBlank
     private String login;
     private String name;
+    @NotNull
     private LocalDate birthday;
 
     public User(String email, String login, String name, LocalDate birthday) {
